72e58973af9788aa1d1cdfcaa65549172db03f8e
Gifty Mensah <giftymens111@gmail.com>
2022-04-07 12:58:53 +0000

    MVC folder structure

---
+++ routes/index.router.js
@@ -0,0 +1,36 @@
+const { Router } = require("express"); //import
+const { greet, welcome } = require("../controllers/index.controller")
+
+const indexRouter = Router(); // instantiate
+
+indexRouter.get("/", welcome);
+
+indexRouter.get("/greet", greet);
+
+
+module.exports = indexRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
